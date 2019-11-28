b4d826beaa2e85d3e6daee4b5f8f7344e4bb17ef
Jakub Wąsik <eghizio@gmail.com>
2019-11-28 20:07:00 +0100

    Load planets progressively

--- src/components/PlanetList.tsx
+++ src/components/PlanetList.tsx
@@ -2,29 +2,32 @@ import React, { useState, useEffect } from "react";
 
 const PlanetList = () => {
     const [planetNames, setPlanetNames] = useState<string[]>([]);
+    
     useEffect(() => {
-        for(let i=1; i<=7; i++)
-            fetch(`/api/planets/?page=${i}`) //proxy "https://swapi.co"
+        const fetchPage = (url:string) => {
+            fetch(url)
                 .catch((err:Error) => console.error(err))
                 .then((res:any) => res.json())
                 .then(data => {
                     const names: string[] = data.results.map((p:{name:string}) => p.name);
                     setPlanetNames(prev => [...prev,...names]);
+                    if(data.next) fetchPage(data.next);
                 });
+        };
+
+        fetchPage("https://swapi.co/api/planets/?page=1");
     },[]);
 
     interface PlanetProps { name: string };
     const Planet: React.FC<PlanetProps> = ({name}) => {
         return (
-            <div style={{width: "300px", margin: "2px auto", borderRadius: "5px", backgroundColor: "skyblue"}}>
+            <div className="planet">
                 {name}
             </div>
         );
     };
 
-    const Loading: React.FC = () => <div>loading . . .</div>;
-
-    if(planetNames.length === 61) console.log(planetNames);
+    const Loading: React.FC = () => <div className="loading">loading . . .</div>;
 
     return (
         <div>
